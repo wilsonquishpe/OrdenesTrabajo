a625426e940a920225128b8abae3077399bcccb7
Daniel Guilcapi <daniel.guilcapi@quito.gob.ec>
2022-05-21 10:15:39 -0500

    first commit

---
+++ OrdenesTrabajo/OrdenesTrabajo/Vistas/Login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdenesTrabajo.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            _ = Navigation.PushAsync(new MenuTab());
+        }
+    }
+}
---
+++ OrdenesTrabajo/OrdenesTrabajo/Vistas/Reportes.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdenesTrabajo.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reportes : ContentPage
+    {
+        public Reportes()
+        {
+            InitializeComponent();
+        }
+    }
+}
